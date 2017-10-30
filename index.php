f2dbafcf44f113e1fbdccc9ca307a98e09d0d297
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-30 13:35:45 +0100

    Variables - Exercice ok

--- index.php
+++ index.php
@@ -5,14 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hi !</title>
+    <title>Accueil</title>
   </head>
 
   <body>
-    <h1>Bonjour
+    <menu>
+      <a href="index.php">Accueil</a> <!-- lien vers la page ind-->
+      <a href="cats.php">Cats</a>
+    </menu>
+
+    <!-- <h1>Bonjour
       <?php echo $_GET['nom']; ?>!
-      Ce code php permet d'ajouter un nom après Bonjour. Ce nom aura transiter via l'url de la page sous la forme de http://localhost/variable/humans.php?nom=Alexandre
-    </h1>
+    </h1> -->
+
+    <p>
+      <?php echo "bonjour";?>
+    </p>
   </body>
 
 </html>
